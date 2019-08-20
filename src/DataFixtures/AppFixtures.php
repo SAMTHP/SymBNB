3cee7a0e97b0c3fa2684acbc188ca438fbc3d741
SAMTHP <samir_615@live.fr>
2019-08-20 14:18:15 +0200

    MAJ

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Faker\Factory;
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -15,15 +16,45 @@ class AppFixtures extends Fixture
         //Initialization of Faker :
         $faker = Factory::create('fr-FR');
 
-        // Set of properties with faker methods
-        $title = $faker->sentence();
-        $cover_image = $faker->imageUrl(1000,350);
-        $introduction = $faker->paragraph(2);
-        $content = '<p>' . join('</p><p>',$faker->paragraphs(5)) . '</p>';
+        // we generate users 
+        $users = [];
 
+        for($e = 1; $e <= 10; $e++){
+            // Firstly we doing an instanciation of Ad class
+            $user = new User();
+
+            // Set of users properties with faker methods            
+            $firstname = $faker->firstName;
+            $lastname = $faker->lastName;
+            $email = $faker->email;
+            $introduction = $faker->sentence();
+            $description = '<p>' . join('</p><p>',$faker->paragraphs(3)) . '</p>';
+
+            // Secondly Configuration of properties of the Ad class
+            $user->setFistName($firstname)
+                 ->setLastName($lastname)
+                 ->setEmail($email)
+                 ->setHash('password')
+                 ->setIntroduction($introduction)
+                 ->setDescription($description);
+
+            // To finish, we using the doctrine manager, in order to persist in to database
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // We generate ads
         for($i = 1; $i <= 30; $i++ ){
             // Firstly we doing an instanciation of Ad class
             $ad = new Ad();
+
+            // Set of properties with faker methods
+            $title = $faker->sentence();
+            $cover_image = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>',$faker->paragraphs(5)) . '</p>';
+
+            $user = $users[mt_rand(0,(count($users)-1))];
 
             // Secondly Configuration of properties of the Ad class
             $ad->setTitle($faker->sentence())
@@ -31,16 +62,18 @@ class AppFixtures extends Fixture
                 ->setIntroduction($faker->paragraph(2))
                 ->setContent('<p>' . join('</p><p>',$faker->paragraphs(5)) . '</p>')
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 5));
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user);
 
+            // We generate images
             for($j = 1; $j <= mt_rand(2, 5) ; $j++){
                 // Firstly we doing an instanciation of Image class
                 $image = new Image();
 
                 // Secondly Configuration of properties of the Image class
                 $image->setCaption($faker->sentence())
-                      ->setUrl($faker->imageUrl())
-                      ->setAd($ad);
+                    ->setUrl($faker->imageUrl())
+                    ->setAd($ad);
                 
                 // To finish, we using the doctrine manager, in order to persist in to database
                 $manager->persist($image);
@@ -50,6 +83,7 @@ class AppFixtures extends Fixture
             // To finish, we using the doctrine manager, in order to persist in to database
             $manager->persist($ad);
         }
+        
 
         $manager->flush();
     }
