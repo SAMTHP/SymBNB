4f1a1e772446e306a80645b4077f9da5eea1402e
SAMTHP <samir_615@live.fr>
2019-08-26 17:01:31 +0200

    MAJ

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use App\Entity\Booking;
 use Faker\Factory;
-use App\Entity\Image;
 use App\Entity\Role;
 use App\Entity\User;
+use App\Entity\Image;
+use App\Entity\Booking;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -165,8 +166,19 @@ class AppFixtures extends Fixture
 
                 // We using the doctrine manager in order to persist this booking in to database
                 $manager->persist($booking);
+
+                // Comments admin
+                if(mt_rand(0,1)){
+                    $comment = new Comment();
+
+                    $comment->setContent($faker->paragraph())
+                            ->setRating(mt_rand(1,5))
+                            ->setAuthor($booker)
+                            ->setAd($ad);
+                    
+                    $manager->persist($comment);
+                }
             }
-            
             // To finish, we using the doctrine manager, in order to persist in to database
             $manager->persist($ad);
         }
