a5ca27c9abad577256dd5a86c6e32b75bb162409
SAMTHP <samir_615@live.fr>
2019-09-01 22:00:02 +0200

    MAJ

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,30 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller{
-
-    /**
-     * Function which the page which say hello
-     * 
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     * 
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0){
-        return $this->render(
-            "hello.html.twig", 
-            [
-                "prenom" => $prenom,
-                "age" => $age
-            ]
-        );
-    }
 
     /**
      * This function allow to show the homepage
@@ -34,18 +17,14 @@ class HomeController extends Controller{
      *
      * @return void
      */
-    public function home(){
-        $prenoms = ["Samir" => 28,"Yasmine" => 23,"Jocker" => 100];
-        $age = 28;
-        $title = "Bonjour à tous";
-        $name = "Samir";
+    public function home(AdRepository $adRepo, UserRepository $userRepo)
+    {
+
         return $this->render(
             "home.html.twig",
             [
-                "title" => $title,
-                "name" => $name,
-                "age" => $age,
-                "prenoms" => $prenoms
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers()
             ]
         );
         
